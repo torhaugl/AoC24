2c0d2df1f0098805bbe2ac88c2a3326fd1eb1476
Tor S. Haugland <tor.haugland@sintef.no>
2024-12-09 22:26:21 +0100

    Day 9 part 1

--- day/9/main.jl
+++ day/9/main.jl
@@ -1,75 +1,78 @@
-function filesystem_checksum(filesystem :: String)
+function filesystem_checksum(filesystem)
     checksum = 0
     for (i, c) in enumerate(filesystem)
-        if c == '.'
+        if c == -1
             continue
         end
-        checksum += (i-1) * parse(Int, c)
+        checksum += (i-1) * c
     end
     return checksum
 end
 
-@assert filesystem_checksum("0099811188827773336446555566..............") == 1928
+# @assert filesystem_checksum("0099811188827773336446555566..............") == 1928
 
 function diskmap_to_filesystem(diskmap)
-    filesystem = ""
+    filesystem = Vector{Int}(undef, 0)
     for (i, c) in enumerate(diskmap)
         num = parse(Int, c)
         if (mod(i, 2) == 1)
             for _ = 1:num
-                filesystem *= string(div(i, 2))
+                push!(filesystem, div(i, 2))
             end
         else
             for _ = 1:num
-                filesystem *= '.'
+                push!(filesystem, -1)
             end
         end
     end
     filesystem
 end
 
-function move_left(str)
-    index_dot = findfirst(==('.'), str)
-    index_num = findlast(isnumeric, str)
-    new_str = str[1:index_dot-1] * str[index_num]
-    new_str *= str[index_dot+1:index_num-1] * '.' * str[index_num+1:end]
-    new_str
+function move_left!(v)
+    index_dot = findfirst(==(-1), v)
+    index_num = findlast(!=(-1), v)
+    v[index_dot] = v[index_num]
+    v[index_num] = -1
 end
 
-str = "0..111....22222"
-new_str = move_left(str)
-true_str = "02.111....2222."
-@assert new_str == true_str
+#str = "0..111....22222"
+#new_str = move_left(str)
+#true_str = "02.111....2222."
+#@assert new_str == true_str
+#
+#str = "02.111....2222."
+#new_str = move_left(str)
+#true_str = "022111....222.."
+#@assert new_str == true_str
+#
+#str = "022111....222.."
+#new_str = move_left(str)
+#true_str = "0221112...22..."
+#@assert new_str == true_str
 
-str = "02.111....2222."
-new_str = move_left(str)
-true_str = "022111....222.."
-@assert new_str == true_str
-
-str = "022111....222.."
-new_str = move_left(str)
-true_str = "0221112...22..."
-@assert new_str == true_str
-
-function amphipod_filesystem(filesystem)
-    filesystem_new = move_left(filesystem)
-    while filesystem_new != filesystem
+function amphipod_filesystem!(filesystem)
+    filesystem_new = copy(filesystem)
+    index_dot = 0
+    index_num = 1
+    while index_num > index_dot
         filesystem = filesystem_new
-        filesystem_new = move_left(filesystem)
+        move_left!(filesystem_new)
+        index_dot = findfirst(==(-1), filesystem_new)
+        index_num = findlast(!=(-1), filesystem_new)
     end
-    return filesystem
+    filesystem = filesystem_new
+    filesystem
 end
 
-@assert diskmap_to_filesystem("12345") == "0..111....22222"
-@assert diskmap_to_filesystem("2333133121414131402") == "00...111...2...333.44.5555.6666.777.888899"
-@show diskmap_to_filesystem("111111111111111111111")
+#@show diskmap_to_filesystem("11111111111111111111111")
+#@assert diskmap_to_filesystem("12345") == "0..111....22222"
+#@assert diskmap_to_filesystem("2333133121414131402") == "00...111...2...333.44.5555.6666.777.888899"
 
 function part1()
-    line = strip(read("day/9/input", String))
-    diskmap = [c for c in line]
+    diskmap = strip(read("day/9/input", String))
     filesystem = diskmap_to_filesystem(diskmap)
     println(filesystem[1:20])
-    filesystem = amphipod_filesystem(filesystem)
+    filesystem = amphipod_filesystem!(filesystem)
     println(filesystem[1:20])
     checksum = filesystem_checksum(filesystem)
     return checksum
